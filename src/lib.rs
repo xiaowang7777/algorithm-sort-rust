160866daeb4aac0c25ff57ba6f469039d0156da4
wangjunfeng <junfeng.wang@pitayagames.com>
2022-04-11 10:44:22 +0800

    插入排序

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,20 @@
 use std::mem;
 
-mod insert;
-mod merge;
-mod selection;
-mod shell;
+pub mod insert;
+pub mod merge;
+pub mod selection;
+pub mod shell;
 
-pub fn less<T>(arr: Vec<T>, lo: usize, hi: usize) -> bool
-where
-    T: PartialOrd,
+pub fn less<T>(arr: &Vec<T>, lo: usize, hi: usize) -> bool
+    where
+        T: PartialOrd,
 {
     return arr.get(lo).expect("") < arr.get(hi).expect("");
 }
 
 pub fn exch<T>(arr: &mut Vec<T>, lo: usize, hi: usize)
-where
-    T: PartialOrd + Default,
+    where
+        T: PartialOrd + Default,
 {
     let tmp_lo = arr.get_mut(lo).map(|tmp| mem::take(tmp)).expect("");
     let tmp_hi = arr.get_mut(hi).map(|tmp| mem::take(tmp)).expect("");
