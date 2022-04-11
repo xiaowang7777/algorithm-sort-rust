0cfba5b98faddf73af2847e9dcf098e75b1725c1
wangjunfeng <junfeng.wang@pitayagames.com>
2022-04-11 11:41:32 +0800

    希尔排序

--- src/shell.rs
+++ src/shell.rs
@@ -1,1 +1,21 @@
+use crate::{exch, less};
 
+pub fn sort<T>(arr: &mut Vec<T>, step: usize)
+    where T: PartialOrd + Default {
+    let mut step_tmp = step;
+
+    while step_tmp > 0 {
+        for i in step_tmp..arr.len() {
+            let mut j = i;
+            while j >= step_tmp {
+                if less(arr, j, j - step_tmp) {
+                    exch(arr, j, j - step_tmp);
+                } else {
+                    break;
+                }
+                j -= step_tmp;
+            }
+        }
+        step_tmp -= 1;
+    }
+}
--- tests/sort_test.rs
+++ tests/sort_test.rs
@@ -8,10 +8,19 @@ fn insert_test() {
 }
 
 #[test]
-fn selection_test(){
+fn selection_test() {
     let mut arr = vec![4, 3, 5, 4, 2, 1];
     let res = vec![1, 2, 3, 4, 4, 5];
     algorithm_sort_rust::selection::sort(&mut arr);
+
+    assert_eq!(arr, res);
+}
+
+#[test]
+fn shell_test() {
+    let mut arr = vec![4, 3, 5, 4, 2, 1];
+    let res = vec![1, 2, 3, 4, 4, 5];
+    algorithm_sort_rust::shell::sort(&mut arr, 2);
 
     assert_eq!(arr, res);
 }
